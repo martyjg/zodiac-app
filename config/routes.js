6a8007d455871ad2a5c470756428735dfe48debb
martyjg <martinjosephgormley@gmail.com>
2015-11-15 12:58:34 +0000

    Added a show route, controller and view page

--- config/routes.js
+++ config/routes.js
@@ -6,4 +6,7 @@ var usersController = require("../controllers/usersController");
 router.route("/users")
   .get(usersController.usersIndex)
 
+router.route("/users/:id")
+  .get(usersController.usersShow)
+
 module.exports = router;
--- controllers/usersController.js
+++ controllers/usersController.js
@@ -7,6 +7,15 @@ function usersIndex(req, res) {
   } )
 }
 
+function usersShow(req, res) {
+  var id = req.params.id;
+  User.findById({ _id: id }, function(err, user) {
+    if (err) return res.render("error", { message: "Something went wrong.. " + err });
+    res.render("users/show", { user : user })
+  } )
+}
+
 module.exports = {
-  usersIndex: usersIndex
+  usersIndex: usersIndex,
+  usersShow: usersShow
 }
